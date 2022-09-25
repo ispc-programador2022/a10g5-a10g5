bd7b6bb83cdc69a0e1102b71dbe2045a478c0d6b
JFP <jotafrancis@protonmail.com>
2022-09-25 11:41:33 -0300

    Primer commmit, Enunciado 1

---
+++ E1. operaciones_entre_dos.py
@@ -0,0 +1,8 @@
+def operaciones_entre_dos():
+    primer = int(input("Primer valor: "))
+    segundo = int(input("Segundo valor: "))
+    print("Primero más el segundo: " + str(primer+segundo))
+    print("Primero menos el segundo: " + str(primer-segundo))
+    print("Primero por el segundo: " + str(primer*segundo))
+    print("Primero entre el segundo: " + str(primer//segundo))
+operaciones_entre_dos()
